efce1a1e0c997fab6d73734b5289c07012a24611
DamianWieszczak <damian.wieszczak@gmail.com>
2022-09-20 20:14:39 +0200

    Update events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,29 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      flash[:success] = "Event '#{@event.title}' created"
+      redirect_to @event
+    else
+      flash.now[:alert] = "Something was wrong with your data"
+      render "new"
+    end
+  end
+
+
+  private
+
+    def event_params
+      params.require(:event).permit(:title, :description, :date, :location)
+    end
 end
