4db2a7d7263f0c7e2a787c16df56d083c009ca56
DamianWieszczak <damian.wieszczak@gmail.com>
2022-09-22 22:04:52 +0200

    Add assocciation between the event attendee and event. Create attendees_events through table

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :events
+  has_many :attendees_events, dependent: :destroy
+  has_many :attended_events, through: :attendees_events, source: :event, dependent: :destroy
 end
